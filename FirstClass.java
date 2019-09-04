b88d3280f990cb2bb4716daff0899e7c66618992
Laura Salcido <idlaurasalcido@gmail.com>
2019-09-03 20:23:57 -0700

    first class
    main

---
+++ FirstClass.java
@@ -0,0 +1,16 @@
+package first_java_project;
+
+public class FirstClass
+{ 
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Hello, World. ");
+        System.out.println("My name is Laura, and I am a Computer Science major. "
+                + "I plan on transfering to UCI for Computer Science");
+        System.out.println("I know multiple languages, but this is my first time working with Java.  ");
+
+    }
+
+}
